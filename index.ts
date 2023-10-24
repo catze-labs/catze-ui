91c454f4b3cec76981710570120d8b3161a6e168
howdyfrom2019 <tjd1emd@naver.com>
2023-10-24 16:46:20 +0900

    Add types

--- index.ts
+++ index.ts
@@ -1,11 +1,38 @@
 /** YOOLDO */
-export { YooldoLogo } from './src/yooldo/components/YooldoLogo';
-export { Button as YooldoButton } from './src//yooldo/components/Button';
-export { Callout as YooldoCallout } from './src//yooldo/components/Callout';
-export { Checkbox as YooldoCheckbox } from './src//yooldo/components/Checkbox';
-export { Divider as YooldoDivder } from './src//yooldo/components/Divider';
-export { GlowingBackground as YooldoGlowingBackground } from './src//yooldo/components/GlowingBackground';
-export { Pagination as YooldoGlowingPagination } from './src//yooldo/components/Pagination';
-export { SelectTextBox as YooldoSelectTextBox } from './src//yooldo/components/SelectTextBox';
-export { TextLink as YooldoTextLink } from './src//yooldo/components/TextLink';
+export {
+  YooldoLogo,
+  type YooldoLogoProps,
+} from './src/yooldo/components/YooldoLogo';
+export {
+  Button as YooldoButton,
+  type ButtonProps as YooldoButtonProps,
+} from './src//yooldo/components/Button';
+export {
+  Callout as YooldoCallout,
+  type CalloutProps as YooldoCalloutProps,
+} from './src//yooldo/components/Callout';
+export {
+  Checkbox as YooldoCheckbox,
+  type CheckboxProps as YooldoCheckboxProps,
+} from './src//yooldo/components/Checkbox';
+export {
+  Divider as YooldoDivder,
+  type DividerProps as YooldoDividerProps,
+} from './src//yooldo/components/Divider';
+export {
+  GlowingBackground as YooldoGlowingBackground,
+  type GlowingProps as YooldoGlowingProps,
+} from './src//yooldo/components/GlowingBackground';
+export {
+  Pagination as YooldoGlowingPagination,
+  type PaginationProps as YooldoPaginationProps,
+} from './src//yooldo/components/Pagination';
+export {
+  SelectTextBox as YooldoSelectTextBox,
+  type SelectBoxProps as YooldoSelectBoxProps,
+} from './src//yooldo/components/SelectTextBox';
+export {
+  TextLink as YooldoTextLink,
+  type TextLinkProps as YooldoTextLinkProps,
+} from './src//yooldo/components/TextLink';
 export { yooldoTailwindConfig } from './src/yooldo/tailwind/config';
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,38 @@
 /** YOOLDO */
-export { YooldoLogo } from './yooldo/components/YooldoLogo';
-export { Button as YooldoButton } from './yooldo/components/Button';
-export { Callout as YooldoCallout } from './yooldo/components/Callout';
-export { Checkbox as YooldoCheckbox } from './yooldo/components/Checkbox';
-export { Divider as YooldoDivder } from './yooldo/components/Divider';
-export { GlowingBackground as YooldoGlowingBackground } from './yooldo/components/GlowingBackground';
-export { Pagination as YooldoGlowingPagination } from './yooldo/components/Pagination';
-export { SelectTextBox as YooldoSelectTextBox } from './yooldo/components/SelectTextBox';
-export { TextLink as YooldoTextLink } from './yooldo/components/TextLink';
+export {
+  YooldoLogo,
+  type YooldoLogoProps,
+} from './yooldo/components/YooldoLogo';
+export {
+  Button as YooldoButton,
+  type ButtonProps as YooldoButtonProps,
+} from './yooldo/components/Button';
+export {
+  Callout as YooldoCallout,
+  type CalloutProps as YooldoCalloutProps,
+} from './yooldo/components/Callout';
+export {
+  Checkbox as YooldoCheckbox,
+  type CheckboxProps as YooldoCheckboxProps,
+} from './yooldo/components/Checkbox';
+export {
+  Divider as YooldoDivider,
+  type DividerProps as YooldoDividerProps,
+} from './yooldo/components/Divider';
+export {
+  GlowingBackground as YooldoGlowingBackground,
+  type GlowingProps as YooldoGlowingProps,
+} from './yooldo/components/GlowingBackground';
+export {
+  Pagination as YooldoPagination,
+  type PaginationProps as YooldoPaginationProps,
+} from './yooldo/components/Pagination';
+export {
+  SelectTextBox as YooldoSelectTextBox,
+  type SelectBoxProps as YooldoSelectBoxProps,
+} from './yooldo/components/SelectTextBox';
+export {
+  TextLink as YooldoTextLink,
+  type TextLinkProps as YooldoTextLinkProps,
+} from './yooldo/components/TextLink';
 export { yooldoTailwindConfig } from './yooldo/tailwind/config';
