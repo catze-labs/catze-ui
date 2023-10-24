fb057860ce0219d79a5be17130e11ae92d44b1f8
Jaake <49380742+howdyfrom2019@users.noreply.github.com>
2023-10-24 15:30:02 +0900

    Fix with lint & export (#2)

--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
 /** YOOLDO */
 export { YooldoLogo } from './src/yooldo/components/YooldoLogo';
+export { Button as YooldoButton } from './src//yooldo/components/Button';
+export { Callout as YooldoCallout } from './src//yooldo/components/Callout';
+export { Checkbox as YooldoCheckbox } from './src//yooldo/components/Checkbox';
+export { Divider as YooldoDivder } from './src//yooldo/components/Divider';
+export { GlowingBackground as YooldoGlowingBackground } from './src//yooldo/components/GlowingBackground';
+export { Pagination as YooldoGlowingPagination } from './src//yooldo/components/Pagination';
+export { SelectTextBox as YooldoSelectTextBox } from './src//yooldo/components/SelectTextBox';
+export { TextLink as YooldoTextLink } from './src//yooldo/components/TextLink';
 export { yooldoTailwindConfig } from './src/yooldo/tailwind/config';
